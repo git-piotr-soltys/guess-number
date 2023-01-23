b32e39a647ccdec13e548db392a6f0fdd0ee9c10
git-piotr-soltys <ja@piotrsoltys.net>
2023-01-23 16:59:19 +0100

    Simple Guess Number game in JAVA

---
+++ src/GuessNumber.java
@@ -0,0 +1,41 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class GuessNumber {
+    public static void main(String[] args) {
+        Random rand = new Random();
+        int newRandomNumber = rand.nextInt(100);
+        System.out.println(" Welcome to a very simple guess number game.");
+        System.out.println(" New integer has been generated. Guess this number.");
+
+        boolean play = true;
+        int counter = 0;
+
+        while(play == true){
+            counter++;
+            System.out.print(" Your guess: ");
+            Scanner userInput = new Scanner(System.in);
+            int yourGuess = 0;
+            try{
+                yourGuess = userInput.nextInt();
+            }
+            catch(Exception e){
+                System.out.println(" Number you have typed is incorrect. Try again.");
+            }
+            finally {
+                if (yourGuess == newRandomNumber) {
+                    System.out.println(" Well done. This is the number.");
+                    System.out.print(" You made it in ");
+                    if(counter==1) System.out.println("1 guess.");
+                    else System.out.println(counter + " guesses.");
+                    play = false;
+                } else if (yourGuess > newRandomNumber) {
+                    System.out.println(" Sorry. Your number is too high. Try again.");
+                } else {
+                    System.out.println(" Sorry. Your number is too low. Try again.");
+                }
+            }
+        }
+
+    }
+}
